3a637f4ce4b1d89b031b59c0a1a5f9927d2ede9b
Murilo-Herrick <muriloherrick@gmail.com>
2025-03-28 19:31:52 -0300

    Aula 10 Ex 5 e 6

---
+++ aula10/A10E05Grids/styles.js
@@ -0,0 +1,33 @@
+import { Platform, StyleSheet, StatusBar } from "react-native";
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: "row", 
+        flexWrap: "wrap", 
+        backgroundColor: "ghostwhite",
+        alignItems: "center", 
+        justifyContent: "center", 
+        ...Platform.select({
+            ios: { paddingTop: 40 }, 
+            android: { paddingTop: StatusBar.currentHeight },
+        }),
+    },
+
+    box: {
+        height: 100,
+        width: 100,
+        justifyContent: "center", 
+        alignItems: "center", 
+        backgroundColor: "lightgray", 
+        borderWidth: 1,
+        borderStyle: "dashed", 
+        borderColor: "darkslategray", 
+        margin: 5,
+    },
+
+    boxText: {
+        color: "darkslategray", 
+        fontWeight: "bold", 
+    },
+});
